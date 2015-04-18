48c5ec33b99b639b9347cdcc997a1af2ceb546ba
tiziano_signo <tiziano.signo@gmail.com>
2015-04-18 23:54:44 +0200

    remove unnecessary

--- backend/keepcalm/keepcalm/urls.py
+++ backend/keepcalm/keepcalm/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
-admin.autodiscover()
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'keepcalm.views.home', name='home'),
