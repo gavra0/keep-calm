dc76b51c83e95b30be8b99f762562900b767b01d
tiziano_signo <tiziano.signo@gmail.com>
2015-04-19 01:17:53 +0200

    remove reference to api_root

--- backend/keepcalm/api/urls.py
+++ backend/keepcalm/api/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from views import api_root, SupportedSiteList, UserList, UserNegativeReport, UserCheck
+from views import SupportedSiteList, UserList, UserNegativeReport, UserCheck
 
 urlpatterns = patterns('',
-	url(r'^$', api_root, name='api_root'),
 	url(r'^site/$', SupportedSiteList.as_view(), name='site_list'),
 	url(r'^user/$', UserList.as_view(), name='user_list'),
 	url(r'^report/$', UserNegativeReport.as_view(), name='user_report'),
