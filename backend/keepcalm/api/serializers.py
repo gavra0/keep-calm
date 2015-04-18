42847ec742de5722ee32c54de9d1de3bcbc805e2
tiziano_signo <tiziano.signo@gmail.com>
2015-04-18 17:01:52 +0200

    add serializers

---
+++ backend/keepcalm/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from rest_framework.reverse import reverse
+
+from core.models import SupportedSite, User
+
+class SupportedSiteSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = SupportedSite
+		fields = ('name', 'base_url', )
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('site', 'username', )
