af28d6af1593d27705cd53b92f041d477176e2ca
tiziano_signo <tiziano.signo@gmail.com>
2015-04-18 17:01:14 +0200

    add migrations and fixtures

---
+++ backend/keepcalm/core/migrations/0001_initial.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SupportedSite',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+                ('base_url', models.CharField(max_length=100)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(max_length=20)),
+                ('positive_reports', models.IntegerField(default=0)),
+                ('negative_reports', models.IntegerField(default=0)),
+                ('site', models.ForeignKey(to='core.SupportedSite')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
