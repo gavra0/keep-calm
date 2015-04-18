4904181c2f3e3aad905821fc04c5336347b06403
tiziano_signo <tiziano.signo@gmail.com>
2015-04-18 14:31:55 +0200

    add basic admin

--- backend/keepcalm/keepcalm/urls.py
+++ backend/keepcalm/keepcalm/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'keepcalm.views.home', name='home'),
